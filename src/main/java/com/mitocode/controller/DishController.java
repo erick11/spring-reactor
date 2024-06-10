9e136ab44eb3d77adcc319bc5f9d806190457548
erick11 <erick.eduardo.ee7@gmail.com>
2024-06-09 19:00:40 -0500

    feat: module 2: Spring hateoas

--- src/main/java/com/mitocode/controller/DishController.java
+++ src/main/java/com/mitocode/controller/DishController.java
@@ -6,6 +6,8 @@ import com.mitocode.service.IDishService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -15,6 +17,10 @@ import reactor.core.publisher.Mono;
 
 import java.net.URI;
 import java.util.function.Function;
+
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
+
 
 @RestController
 @RequestMapping("/dishes")
@@ -26,10 +32,10 @@ public class DishController {
     private final ModelMapper modelMapper;
 
     @GetMapping
-    public Mono<ResponseEntity<Flux<DishDTO>>> findAll (){
+    public Mono<ResponseEntity<Flux<DishDTO>>> findAll() {
         // .map: pide como parametro una interface funcional Function
         // Function: T parametro de entrada y R es el resultado => e -> modelMapper.map(e, DishDTO.class)
-        Flux<DishDTO>  fx = service.findAll().map(this::converToDTO);
+        Flux<DishDTO> fx = service.findAll().map(this::converToDTO);
         return Mono.just(ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(fx)
@@ -37,7 +43,7 @@ public class DishController {
     }
 
     @GetMapping("/{id}")
-    public Mono<ResponseEntity<DishDTO>> finById (@PathVariable("id") String id){
+    public Mono<ResponseEntity<DishDTO>> finById(@PathVariable("id") String id) {
 
         return service.findById(id)
                 .map(this::converToDTO)
@@ -54,7 +60,7 @@ public class DishController {
     }
 
     @GetMapping("/v2/{id}")
-    public Mono<ResponseEntity<Mono<Dish>>> finByIdV2 (@PathVariable("id") String id){
+    public Mono<ResponseEntity<Mono<Dish>>> finByIdV2(@PathVariable("id") String id) {
         return service.findById(id)
                 //Tranformar la salida anterior a un ResponseEntity
                 .map(e -> ResponseEntity.ok()
@@ -70,19 +76,19 @@ public class DishController {
 
 
     @PostMapping
-    public Mono<ResponseEntity<DishDTO>> save (@Valid @RequestBody DishDTO dto, final ServerHttpRequest req){
-        return service.save( converToDocument(dto))
+    public Mono<ResponseEntity<DishDTO>> save(@Valid @RequestBody DishDTO dto, final ServerHttpRequest req) {
+        return service.save(converToDocument(dto))
                 .map(this::converToDTO)
                 .map(e -> ResponseEntity.created(
-                                      URI.create(req.getURI().toString().concat("/").concat(e.getId()))
-                              )
-                              .contentType(MediaType.APPLICATION_JSON)
-                              .body(e)
+                                        URI.create(req.getURI().toString().concat("/").concat(e.getId()))
+                                )
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(e)
                 ).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    public Mono<ResponseEntity<DishDTO>> update (@Valid @PathVariable("id") String id, @RequestBody DishDTO dto){
+    public Mono<ResponseEntity<DishDTO>> update(@Valid @PathVariable("id") String id, @RequestBody DishDTO dto) {
         return Mono.just(dto)
                 .map(e -> {
                     e.setId(id);
@@ -98,15 +104,45 @@ public class DishController {
     }
 
     @DeleteMapping("/{id}")
-    public Mono<ResponseEntity<Void>> delete(@PathVariable("id") String id){
+    public Mono<ResponseEntity<Void>> delete(@PathVariable("id") String id) {
         return service.delete(id)
                 .flatMap(result -> {
-                   if(result){
-                       return Mono.just(ResponseEntity.noContent().build());
-                   } else{
-                       return Mono.just(ResponseEntity.notFound().build());
-                   }
+                    if (result) {
+                        return Mono.just(ResponseEntity.noContent().build());
+                    } else {
+                        return Mono.just(ResponseEntity.notFound().build());
+                    }
                 });
+    }
+
+    private DishDTO dishHateoas;
+    @GetMapping("/hateoas/{id}")
+    public Mono<EntityModel<DishDTO>> getHateoas (@PathVariable ("id") String id) {
+        Mono <Link> monoLink =  linkTo(methodOn(DishController.class).finById(id)).withRel("dish-info").toMono();
+
+        //PRACTICA ES COMUN PERO NO RECOMENADA
+        /*
+        return service.findById(id)
+                .map(this::converToDTO)
+                .flatMap(d -> {
+                    this.dishHateoas = d;
+                    return monoLink;
+                })
+                .map(link -> EntityModel.of(this.dishHateoas,link));
+         */
+
+        //PRACTICA INTERMEDIA
+        /*
+        return service.findById(id)
+                .map(this::converToDTO)
+                .flatMap(d -> monoLink.map(link -> EntityModel.of(d, link)));
+         */
+        //PRACTICA IDEAL
+        return service.findById(id)
+                .map(this::converToDTO)
+                .zipWith(monoLink, EntityModel::of); //.zipWith(monoLink, (d, link) -> EntityModel.of(d, link));
+
+        //return EntityModel.of(objeto, link);
     }
 
     private DishDTO converToDTO (Dish model){
